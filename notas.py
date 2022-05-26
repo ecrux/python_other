85953d7e2a33b81110f5c773f60f972be7cf7ba6
Edwar Cruz <estebancruz373@gmail.com>
2022-05-25 23:26:30 -0500

    first commit

---
+++ notas.py
@@ -0,0 +1,14 @@
+# Tipos*
+# String
+# Boolean
+# Listas / arreglos
+# Numeros / float
+
+# Estatico = Levantan los errores de tipo en tiempo de compilacion, 
+# el programa no se ejecuta hasta que solucione el error en el tipado
+
+# Debil = trata de manera poco estricta el manejo de los tipos de datos 
+
+# Dinamico = Muestra los errores de tipado en tiempo de ejecucion
+#  python
+# Fuerte = trata de manera muy fuerte el tipo de datos
